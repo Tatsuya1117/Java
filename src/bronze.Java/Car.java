980515b76e80dcaf3474ae833daa77c386bb604f
Tatsuya1117 <othe.world.pc@gmail.com>
2022-01-27 21:04:44 +0900

    Car.javaを作成

---
+++ src/bronze.Java/Car.java
@@ -0,0 +1,13 @@
+package bronze.Java;
+
+class Car {
+	public static int count = 0;
+	public static String name = "HONDA";
+
+
+	public static void main(String[] args) {
+		System.out.println(Car.name);
+
+	}
+
+}
