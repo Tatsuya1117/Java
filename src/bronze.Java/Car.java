9f306cd341dae23fa1999fbcfd9f8aee446c6956
Tatsuya1117 <yk.tatsuyaozawa@gmail.com>
2022-01-31 23:48:11 +0000

    Car.javaを作成

--- src/bronze.Java/Car.java
+++ src/bronze.Java/Car.java
@@ -1,13 +1,76 @@
+Last login: Mon Jan 31 19:28:27 on ttys000
+/Users/ykmac/.zshrc:source:5: too many open files: /Users/ykmac/.zshrc
+/Users/ykmac/.zshrc:source:8: too many open files: /Users/ykmac/.fzf.zsh
+ykmac@YK-MBP ~ % ssh tatsuya@localhost
+tatsuya@localhost's password:
+Welcome to Ubuntu 20.04.3 LTS (GNU/Linux 5.4.0-96-generic aarch64)
+
+ * Documentation:  https://help.ubuntu.com
+ * Management:     https://landscape.canonical.com
+ * Support:        https://ubuntu.com/advantage
+
+  System information as of Mon 31 Jan 2022 11:43:40 PM UTC
+
+  System load:              0.33
+  Usage of /:               32.8% of 30.63GB
+  Memory usage:             10%
+  Swap usage:               0%
+  Processes:                241
+  Users logged in:          1
+  IPv4 address for enp0s10: 10.0.2.15
+  IPv6 address for enp0s10: fec0::381b:f0ff:fe4a:5092
+
 package bronze.Java;
 
 class Car {
-	public static int count = 0;
-	public static String name = "HONDA";
+        public static int count = 0;
+        public static String name = "HONDA";
 
 
-	public static void main(String[] args) {
-		System.out.println(Car.name);
+        public static void main(String[] args) {
+                System.out.println(Car.name);
 
-	}
+        }
 
 }
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"Car.java" 13L, 185C                                          1,1           All
+Last login: Mon Jan 31 19:28:27 on ttys000
+  System information as of Mon 31 Jan 2022 11:43:40 PM UTC
+
+        }
+package bronze.Java;
+
+class Car {
+        public static int count = 0;
+        public static String name = "HONDA";
+
+
+        public static void main(String[] args) {
+                System.out.println(Car.name);
+
+        }
+
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+-- INSERT --                                                  1,1           All
--- src/bronze.Java/Main.java
+++ src/bronze.Java/Main.java
@@ -1,3 +1,59 @@
+Last login: Mon Jan 31 19:28:27 on ttys000
+/Users/ykmac/.zshrc:source:5: too many open files: /Users/ykmac/.zshrc
+/Users/ykmac/.zshrc:source:8: too many open files: /Users/ykmac/.fzf.zsh
+ykmac@YK-MBP ~ % ssh tatsuya@localhost
+tatsuya@localhost's password:
+Welcome to Ubuntu 20.04.3 LTS (GNU/Linux 5.4.0-96-generic aarch64)
+
+ * Documentation:  https://help.ubuntu.com
+ * Management:     https://landscape.canonical.com
+ * Support:        https://ubuntu.com/advantage
+
+  System information as of Mon 31 Jan 2022 11:43:40 PM UTC
+
+  System load:              0.33
+  Usage of /:               32.8% of 30.63GB
+  Memory usage:             10%
+  Swap usage:               0%
+  Processes:                241
+  Users logged in:          1
+  IPv4 address for enp0s10: 10.0.2.15
+  IPv6 address for enp0s10: fec0::381b:f0ff:fe4a:5092
+
+package bronze.Java;
+
+class Car {
+        public static int count = 0;
+        public static String name = "HONDA";
+
+
+        public static void main(String[] args) {
+                System.out.println(Car.name);
+
+        }
+
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"Car.java" 13L, 185C                                          1,1           All
+Last login: Mon Jan 31 19:28:27 on ttys000
+  System information as of Mon 31 Jan 2022 11:43:40 PM UTC
+
+        }
+
+~
+
+ * Super-optimized for small spaces - read how we shrank the memory
+   footprint of MicroK8s to make it the smallest full K8s around.
 package bronze.Java;
 
 import java.util.Scanner;
@@ -5,7 +61,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World !!");
-    
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("入力してください > ");
@@ -15,8 +71,36 @@ public class Main {
         System.out.println(input_text + "が入力されました。");
 
         scanner.close();
-    
+
     }
 
 }
 
+~
+~
+"Main.java" 22L, 438C                                         1,1           All
+package bronze.Java;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World !!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("入力してください > ");
+
+        String input_text = scanner.nextLine();
+
+        System.o
+~
+~
+~
+~
+~
+~
+~
+~
+~
+-- INSERT --                                                  15,17         All
