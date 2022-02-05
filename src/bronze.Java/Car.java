d5deeb85a96ed2c83fc976bb6bcdebb1dd4e99c3
Tatsuya1117 <yk.tatsuyaozawa@gmail.com>
2022-02-05 13:43:34 +0000

    car.javaを変更

--- src/bronze.Java/Car.java
+++ src/bronze.Java/Car.java
@@ -1,76 +1,47 @@
-Last login: Mon Jan 31 19:28:27 on ttys000
-/Users/ykmac/.zshrc:source:5: too many open files: /Users/ykmac/.zshrc
-/Users/ykmac/.zshrc:source:8: too many open files: /Users/ykmac/.fzf.zsh
-ykmac@YK-MBP ~ % ssh tatsuya@localhost
-tatsuya@localhost's password:
-Welcome to Ubuntu 20.04.3 LTS (GNU/Linux 5.4.0-96-generic aarch64)
-
- * Documentation:  https://help.ubuntu.com
- * Management:     https://landscape.canonical.com
- * Support:        https://ubuntu.com/advantage
-
-  System information as of Mon 31 Jan 2022 11:43:40 PM UTC
-
-  System load:              0.33
-  Usage of /:               32.8% of 30.63GB
-  Memory usage:             10%
-  Swap usage:               0%
-  Processes:                241
-  Users logged in:          1
-  IPv4 address for enp0s10: 10.0.2.15
-  IPv6 address for enp0s10: fec0::381b:f0ff:fe4a:5092
-
 package bronze.Java;
 
 class Car {
-        public static int count = 0;
-        public static String name = "HONDA";
+	public static int Price = 0;
+	public String Company;
+	public String Name;
+	public String Color;
+	public int tire;
+	public int age;
+	public double height, weight;
 
-
-        public static void main(String[] args) {
-                System.out.println(Car.name);
-
-        }
+	Car(String Company, String Name, String Color, int age, double height, double weight) {
+		Car.Price++;
+		this.Company = Company;
+		this.Name = Name;
+		this.Color = Color;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+	}
+	Car(String Company, String Name, String Color, int tire, int age, double height, double weight) {
+		Car.Price++;
+		this.Company = Company;
+		this.Name = Name;
+		this.Color = Color;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+		this.tire = tire;
+	}
+	public String Company() {
+		return this.Company + " " + this.Name;
+	}
+	public double TotalAmount() {
+		return this.weight * height;
+	}
+	public void printDate() {
+		System.out.println("車は" + this.Company() + "です。");
+		System.out.println("製造から" + this.age + "年、経過しています。");
+		System.out.println("タイヤは" + this.tire + "本あります。");
+		System.out.println("車両の重さは" + Math.round(this.TotalAmount()) + "tです");
+	}
+	public static void printCount() {
+		System.out.println("値段は" + Car.Price + "台です。");
+	}
 
 }
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-"Car.java" 13L, 185C                                          1,1           All
-Last login: Mon Jan 31 19:28:27 on ttys000
-  System information as of Mon 31 Jan 2022 11:43:40 PM UTC
-
-        }
-package bronze.Java;
-
-class Car {
-        public static int count = 0;
-        public static String name = "HONDA";
-
-
-        public static void main(String[] args) {
-                System.out.println(Car.name);
-
-        }
-
-}
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
--- INSERT --                                                  1,1           All
