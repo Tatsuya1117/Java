695cfea0d36265baba9247dc132b7a1b7687eaf6
Tatsuya1117 <othe.world.pc@gmail.com>
2022-02-10 07:54:53 +0000

    Person.javaにプセル化を記述

--- src/progate/Person.java
+++ src/progate/Person.java
@@ -1,11 +1,11 @@
 class Person {
-  public static int count = 0;
-  public String firstName;
-  public String middleName;
-  public String lastName;
-  public int age;
-  public double height;
-  public double weight;
+  private static int count = 0;
+  private String firstName;
+  private String middleName;
+  private String lastName;
+  private int age;
+  private double height;
+  private double weight;
 
   Person(String firstName, String lastName, int age, double height, double weight) {
     Person.count++;
@@ -21,10 +21,14 @@ class Person {
     this.middleName = middleName;
   }
 
+  // middleNameフィールドのゲッターを定義してください
+  public String getMiddleName() {
+    return this.middleName;
+  }
+
   public String fullName() {
-    // 以下を、middleNameがない場合とある場合で条件分岐を行ってください
-    if(this.middleName == null) {
-    return this.firstName + " " + this.lastName;
+    if (this.middleName == null) {
+      return this.firstName + " " + this.lastName;
     } else {
       return this.firstName + " " + this.middleName + " " + this.lastName;
     }
@@ -33,8 +37,6 @@ class Person {
   public void printData() {
     System.out.println("私の名前は" + this.fullName() + "です");
     System.out.println("年齢は" + this.age + "歳です");
-    System.out.println("身長は" + this.height + "mです");
-    System.out.println("体重は" + this.weight + "kgです");
     System.out.println("BMIは" + Math.round(this.bmi()) + "です");
   }
 
