8d3a10afab561c91afd18c894c1dfdea1bbe1e4d
Tatsuya1117 <othe.world.pc@gmail.com>
2022-02-15 11:57:00 +0000

    se78_8_39.javaを追加

---
+++ src/bronze.Java/se78_8_39.java
@@ -0,0 +1,23 @@
+package bronze.Java;
+
+public class se78_8_39 {
+
+	public static void main(String[] args) {
+		char c = 'b';
+		switch (c) {
+		case 'a':
+			System.out.println("A");
+			break;
+		case 'b':
+			System.out.println("B");
+		case 'c':
+			System.out.println("C");
+			break;
+		default:
+			System.out.println("D");
+			break;
+		}
+
+	}
+
+}
