96bc167c4d072523f4198715830690062ea0599d
Tatsuya1117 <othe.world.pc@gmail.com>
2022-02-16 00:41:06 +0000

    se78_8_31.javaを検証

---
+++ src/bronze.Java/se78_8_31.java
@@ -0,0 +1,16 @@
+package bronze.Java;
+
+//以下、3行目でコンパイルエラー
+
+//public class se78_8_31 {
+//
+//	public static void main(String[] args) {
+//		Item[] items = {new Item(), new Item(), new Item()};
+//		int u = item.length;
+//		do while (u > 0) {
+//			System.out.println(u-- + " ");
+//		}
+//
+//	}
+//
+//}
