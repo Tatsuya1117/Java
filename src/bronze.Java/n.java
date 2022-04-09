62f3f4b6bccf51d036df1652a41e6bb29c927b89
Tatsuya1117 <othe.world.pc@gmail.com>
2022-04-09 15:33:08 +0900

    今日の学習20220409

---
+++ src/bronze.Java/n.java
@@ -0,0 +1,12 @@
+public class n {
+    public static void main(String[] args) {
+        int sum = 0;
+        int i = 1;
+
+        while (i <= 89 ) {
+            sum = sum + i;
+            System.out.println(sum);
+            i++;
+        }
+    }
+}
