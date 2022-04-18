da8553a673f845643189a5c68f592fedc149e13c
Tatsuya1117 <othe.world.pc@gmail.com>
2022-04-19 08:46:00 +0900

    VariableSample.javaを作成

---
+++ src/gentleJava/VariableSample.java
@@ -0,0 +1,10 @@
+package gentleJava;
+
+public class VariableSample {
+    public static void main(String[] args) {
+        int num = 10;
+        System.out.println(num);
+        num = 20;
+        System.out.println(num);
+    }
+}
