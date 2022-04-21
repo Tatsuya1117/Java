d37901ce7ea7262e9e8a2830b99caa731aad223b
Tatsuya1117 <othe.world.pc@gmail.com>
2022-04-21 16:16:00 +0900

    example2を追加

--- src/example/Cat.java
+++ src/example/Cat.java
@@ -2,17 +2,44 @@ package example;
 
 public class Cat {
 	String name;
+	int age;
 	boolean hungry;
 
 	void eat() {
 		System.out.println(name + " > ご飯を食べるよ!おいしいにゃ-");
+		System.out.println(name + "> お腹がいっぱいになったにゃー");
+		hungry = false;
 	}
 
 	boolean isHungry() {
-		return true;
+		return hungry;
 	}
 	
 	void playToy(String toy) {
 		System.out.println(name + "> " + toy + "で遊ぶよ。楽しいにゃー");
+		System.out.println(name + "> 遊んでお腹が減ったにゃー");
+		hungry = true;
+	}
+	
+	void setName(String catName) {
+		name = catName;
+	}
+	
+	String getName() {
+		return name;
+	}
+	
+	void setAge(int catAge) {
+		age = catAge;
+	}
+	
+	int getAge() {
+		return age;
+	}
+	
+	void introduceMyself() {
+		String n = getName();
+		int a = getAge();
+		System.out.println("名前は" + n + "です、" + a + "歳です。");
 	}
 }
---
+++ src/example/UseCatSample2.java
@@ -0,0 +1,17 @@
+package example;
+
+public class UseCatSample2 {
+
+	public static void main(String[] args) {
+		Cat tama = new Cat();
+		Cat mike = new Cat();
+		
+		tama.setName("タマ");
+		tama.setAge(3);
+		mike.setName("みけ");
+		mike.setAge(4);
+		tama.introduceMyself();
+		mike.introduceMyself();
+	}
+
+}
